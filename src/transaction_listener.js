3d347d49aa746488e81caf77277a008b85e289c9
Parsa Yazdani <3parsa3@gmail.com>
2024-04-10 19:11:32 +1000

    Refactor, fix mem leak, fix bugs, optimise, etc

---
+++ src/transaction_listener.js
@@ -0,0 +1,114 @@
+import fs from "fs";
+import dgram from "dgram";
+
+const receiveTransaction = (torrentClient, infohash) => {
+    console.log("\nReceived transaction:", infohash);
+    
+    if (infohash.length !== 40 || !/^[0-9A-Fa-f]+$/.test(infohash)) {
+        console.log("Invalid infohash");
+        return;
+    }
+
+    if (fs.existsSync(`torrents/${infohash}.torrent`)) {
+        console.log('Torrent already known');
+        return;
+    }
+
+    const matchedTorrents = torrentClient.torrents.filter(torrent => torrent.path === `mempool/${infohash}`);
+    if (matchedTorrents.length > 0){
+        console.log('Torrent is already downloading');
+        return;
+    }
+
+    console.log('Downloading Metadata', infohash)
+    torrentClient.add(infohash, {path: `mempool/${infohash}`, announce: ['udp://tracker.openbittorrent.com:80', 'wss://tracker.openwebtorrent.com/', 'wss://tracker.webtorrent.dev', 'wss://tracker.files.fm:7073/announce', 'ws://tracker.files.fm:7072/announce']}, (torrent) => {
+        console.log('Downloading Transaction:', torrent.infoHash);
+        torrent.on('done', function () {
+            console.log('Client has finished downloading:', torrent.infoHash);
+
+            fs.readdir(`mempool/${infohash}`, (err, files) => {
+                for (const i in files) {
+                    const file = files[i];
+                    fs.readFile(`mempool/${infohash}/${file}`, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error(err);
+                            return;
+                        }
+                        const {tx, signature, hash} = JSON.parse(data);
+                        if (wallet.validateTransaction(tx, signature, hash)) {
+                            console.log("Valid Transaction");
+                            fs.writeFileSync(`transactions/${hash}.json`, data);
+                            fs.writeFileSync(`torrents/${infohash}.torrent`, torrent.torrentFile);
+                        }
+                    });
+                };
+            });
+        });
+    });
+}
+
+export default function transactionListener(torrentClient, listenPort) {
+    const dgramClient = dgram.createSocket('udp4');
+
+    setInterval(() => {
+        fetch('https://ttx-dht.starfiles.co/transactions.txt?c=' + Math.random()).then(response => response.text()).then(data => {
+            const infohashes = data.split('\n');
+            for (const i in infohashes) {
+                if (!fs.existsSync(`torrents/${infohashes[i]}.torrent`))
+                    receiveTransaction(torrentClient, infohashes[i]);
+            }
+        });
+    }, 30000);
+
+    dgramClient.on('listening', () => {
+        const address = dgramClient.address();
+        console.log(`Client listening ${address.address}:${address.port}`);
+
+        const torrents = fs.readdirSync('torrents').map(file => file.replace('.torrent', ''));
+        const peers = fs.readFileSync('./peers.txt').toString().split('\n');
+
+        for (const i in peers) {
+            const peer = peers[i].split(':');
+            dgramClient.send(JSON.stringify({torrents, peers}), peer[1], peer[0], (err) => {
+                if (err) {
+                    console.error(err);
+                    dgramClient.close();
+                }
+            });
+        }
+    });
+
+    dgramClient.on('message', (msg, rinfo) => {
+        const payload = JSON.parse(msg.toString());
+        console.log(`Received payload from ${rinfo.address}:${rinfo.port}`);
+
+        if(payload['peers']) {
+            const uniquePeers = new Set([...fs.readFileSync('./peers.txt').toString().split('\n'), ...payload['peers']]);
+            fs.writeFileSync('./peers.txt', Array.from(uniquePeers).join('\n'));
+        }
+
+        if(payload['torrents']) {
+            for (const i in payload['torrents']) {
+                const infohash = payload['torrents'][i];
+                receiveTransaction(torrentClient, infohash);
+            }
+        }
+
+        if(!payload['pong']) {
+            const response = {}
+
+            response['torrents'] = fs.readdirSync('./torrents').map(file => file.replace('.torrent', ''));
+            response['peers'] = fs.readFileSync('./peers.txt').toString().split('\n');
+            response['pong'] = true;
+
+            dgramClient.send(JSON.stringify(response), rinfo.port, rinfo.address, (err) => {
+                if (err) {
+                    console.error(err);
+                    dgramClient.close();
+                }
+            });
+        }
+    });
+
+    dgramClient.bind(listenPort);
+}
---
+++ src/wallet.js
@@ -0,0 +1,176 @@
+import fs from 'fs';
+import ethUtil from 'ethereumjs-util';
+import bip39 from 'bip39';
+import HDWallet from 'ethereumjs-wallet';
+
+const hdkey = HDWallet.hdkey;
+
+export default class Wallet {
+    constructor(walletFilePath = 'wallet.json') {
+        this.walletFilePath = walletFilePath;
+
+        const keys = fs.existsSync(this.walletFilePath) ? this.loadKeys() : this.createKeys();
+        for (const key in keys) {
+            this[key] = keys[key];
+        }
+        this.getBalances()
+    }
+
+    generateAddress(path = "m/44'/60'/0'/0/0") {
+        this.seed = bip39.mnemonicToSeedSync(this.mnemonic);
+        this.hdWallet = hdkey.fromMasterSeed(this.seed);
+        this.key = this.hdWallet.derivePath(path);
+        this.wallet = this.key.getWallet();
+    }
+
+    createKeys() {
+        console.log("Creating Keys");
+        this.mnemonic = bip39.generateMnemonic();
+
+        const walletData = {
+            mnemonic: this.mnemonic
+        };
+
+        fs.writeFileSync(this.walletFilePath, JSON.stringify(walletData, null, 4));
+        return walletData;
+    }
+
+    loadKeys() {
+        console.log("Loading Keys");
+        return JSON.parse(fs.readFileSync(this.walletFilePath));
+    }
+
+    signMessage(message) {
+        // Ensure the message is a Buffer
+        const messageBuffer = Buffer.from(message);
+        // Hash the message to get a fixed-length hash
+        const messageHash = ethUtil.keccak256(messageBuffer);
+        // Get the private key from the wallet in Buffer format
+        const privateKeyBuffer = this.wallet.getPrivateKey();
+        // Sign the message hash using the private key
+        const signatureObject = ethUtil.ecsign(messageHash, privateKeyBuffer);
+        // Convert the signature to a hex string format
+        const signatureHex = ethUtil.toRpcSig(signatureObject.v, signatureObject.r, signatureObject.s);
+        return signatureHex;
+    }
+
+    verifySignature(message, signature, walletAddress) {
+        const messageBuffer = Buffer.from(message);
+        const messageHash = ethUtil.keccak256(messageBuffer);
+
+        // Split the signature into its components
+        const signatureBuffer = ethUtil.toBuffer(signature);
+        const signatureParams = ethUtil.fromRpcSig(signatureBuffer);
+
+        // Use ecrecover to obtain the public key that made the signature
+        const publicKey = ethUtil.ecrecover(messageHash, signatureParams.v, signatureParams.r, signatureParams.s);
+
+        // Get the wallet address from the public key
+        const addressBuffer = ethUtil.pubToAddress(publicKey);
+        const address = ethUtil.bufferToHex(addressBuffer);
+
+        // Now, compare this address with the expected address
+        // Assuming `walletAddress` is the address you expect the signature to come from
+        return address.toLowerCase() === walletAddress.toLowerCase();
+    }
+
+    findUnusedUTXOs(address, amount) {
+        const UTXOs = [];
+        const transactions = fs.readdirSync('transactions');
+        for (const i in transactions) {
+            const file = transactions[i];
+            const data = JSON.parse(fs.readFileSync(`transactions/${file}`));
+            const { tx, signature, hash } = data;
+            if (tx.to === address)
+                UTXOs.push({tx, signature, hash});
+        }
+
+        // sort UTXOs by amount, lowest first
+        UTXOs.sort((a, b) => a.tx.amount - b.tx.amount);
+
+        // Loop through UTXOs from smallest to largest till you find one bigger than amount
+        const selectedUTXOs = [];
+        for (const i in UTXOs) {
+            const UTXO = UTXOs[i];
+            if (UTXO.tx.amount >= amount){
+                selectedUTXOs.push(UTXO.hash);
+                break;
+            }
+        }
+
+        if (selectedUTXOs.length === 0)
+            console.log("Can't find UTXO  (error de-escalated to warning on devnet)");
+        //     throw new Error("Can't find UTXO")
+
+        return selectedUTXOs;
+    }
+
+    createTransaction(from, to, amount, message) {
+        const tx = {
+            nonce: Math.random(),
+            from: from,
+            to: to,
+            amount: amount / 1,
+            message: message,
+            prev: this.findUnusedUTXOs(from, amount),
+        }
+        const txString = JSON.stringify(tx);
+
+        const hash = ethUtil.sha256(Buffer.from(txString)).toString('hex');
+
+        const signature = this.signMessage(hash);
+        return {
+            tx,
+            signature,
+            hash
+        }
+    }
+
+    getBalances() {
+        // Get the balance of the address from transactions dir
+        const transactions = fs.readdirSync('transactions');
+        const balances = {};
+        // const remaining_utxos = {};
+        for (const i in transactions) {
+            const file = transactions[i];
+            const path = `transactions/${file}`;
+            const { tx, signature, hash } = JSON.parse(fs.readFileSync(path).toString());
+            const isValid = this.validateTransaction(tx, signature, hash);
+
+            if (isValid) {
+                // if (remaining_utxos[hash])
+                //     remaining_utxos[hash] += tx.amount;
+                // else
+                //     remaining_utxos[hash] = tx.amount;
+                // for (const i in tx.prev) {
+                //     const hash = tx.prev[i];
+                //     if (remaining_utxos[hash]) {
+                //         remaining_utxos[hash] -= tx.amount;
+                //     } else {
+                //         remaining_utxos[hash] = -tx.amount;
+                //     }
+                // }
+
+                if (balances[tx.to])
+                    balances[tx.to] += tx.amount;
+                else
+                    balances[tx.to] = tx.amount;
+
+                if (hash !== "a6d501fece933802ec51863c1248c5a29ec834bf392cc5eb919e7801da5d2284") { // If not genesis
+                    if (balances[tx.from])
+                        balances[tx.from] -= tx.amount;
+                    else
+                        balances[tx.from] = -tx.amount;
+                }
+            } else {
+                console.log("Invalid transaction");
+            }
+        }
+        this.balances = balances;
+        return this.balances;
+    }
+
+    validateTransaction(tx, signature, hash) {
+        return this.verifySignature(hash, signature, tx.from);
+    }
+}
