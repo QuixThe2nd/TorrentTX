c1108adc8a71c04945cfec82377c78277f1412c0
Parsa Yazdani <3parsa3@gmail.com>
2024-04-14 20:35:15 +1000

    Update wire.js

--- src/wire.js
+++ src/wire.js
@@ -17,7 +17,7 @@ export default (clients) => {
   	  	}
 
   	  	onExtendedHandshake(handshake) {
-			console.log("Extended handshake with:", handshake.m.name);
+			console.log("Extended handshake with:", handshake.m);
   	  	  	if (!handshake.m || !handshake.m.torrenttx)
   	  	  	  	return this.emit('warning', new Error('Peer does not support torrenttx'))
 
