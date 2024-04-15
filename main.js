62d282db32f8c76bc88b47bde4f9e6f69135c97b
Parsa Yazdani <3parsa3@gmail.com>
2024-04-15 19:26:39 +1000

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ clients.wallet = new Wallet(clients);
 clients.dgram = dgram.createSocket('udp4');
 clients.transactions = new Transactions(clients);
 clients.transactions.loadSavedTransactions();
-clients.wire = new Wire(clients);
+clients.wire = Wire(clients);
 
 clients.webtorrent.on('listening', () => {
     const address = clients.webtorrent.address();
