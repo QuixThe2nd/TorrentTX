f1d93c4b92187b591849e28e2f44eb92215b12f8
Parsa Yazdani <3parsa3@gmail.com>
2024-04-23 04:37:43 +1000

    clean up

--- ui/preload.js
+++ ui/preload.js
@@ -1,9 +1,7 @@
-const { contextBridge, ipcRenderer } = require('electron');
+const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld(
-    'electronAPI', {
         handleMessage: (callback) => ipcRenderer.on('message', callback),
         handleLog: (callback) => ipcRenderer.on('log', callback),
         sendMessageToMain: (message) => ipcRenderer.send('message-from-renderer', message),
-    }
-);
+contextBridge.exposeInMainWorld('electronAPI', {
+})
