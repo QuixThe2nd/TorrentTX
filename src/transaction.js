1047c0fb051609d42394d1d47f8014ff2b575992
Parsa Yazdani <3parsa3@gmail.com>
2024-04-23 04:25:37 +1000

    Update transaction.js

--- src/transaction.js
+++ src/transaction.js
@@ -1,67 +1,60 @@
-import fs from 'fs';
-import fetch from 'node-fetch';
-import ethUtil from 'ethereumjs-util';
-import Wire from './wire.js';
+import fs from 'fs'
+import fetch from 'node-fetch'
+import ethUtil from 'ethereumjs-util'
+import Wire from './wire.js'
 
 export default class Transaction {
-    constructor(clients, {from, to, amount, message, hash, infohash, torrentPath, path}) {
-        this.clients = clients;
-        this.genesisHash = fs.readFileSync('genesis.txt').toString().trim();
-        this.getTrackers();
+  constructor (clients, { from, to, amount, message, hash, infohash, torrentPath, path }) {
+    this.clients = clients
+    this.genesisHash = fs.readFileSync('genesis.txt').toString().trim()
+    this.getTrackers()
 
-        if (hash) {
-            this.hash = hash;
-            this.txContentString = fs.readFileSync(`transactions/${hash}.json`).toString();
-            this.content = JSON.parse(this.txContentString);
-            this.body = this.content.tx;
-            this.signature = this.content.signature;
+    if (hash) {
+      this.hash = hash
+      this.txContentString = fs.readFileSync(`transactions/${hash}.json`).toString()
+      this.content = JSON.parse(this.txContentString)
+      this.body = this.content.tx
+      this.signature = this.content.signature
 
-            this.validateAndSaveTransaction();
-        } else if (path) {
-            this.txContentString = fs.readFileSync(path).toString();
-            this.content = JSON.parse(this.txContentString);
-            this.body = this.content.tx;
-            this.hash = this.content.hash;
-            this.signature = this.content.signature;
+      this.validateAndSaveTransaction()
+    } else if (path) {
+      this.txContentString = fs.readFileSync(path).toString()
+      this.content = JSON.parse(this.txContentString)
+      this.body = this.content.tx
+      this.hash = this.content.hash
+      this.signature = this.content.signature
 
-            this.validateAndSaveTransaction();
-        } else if (infohash) {
-            if (!infohash || infohash.length !== 40 || !/^[0-9A-Fa-f]+$/.test(infohash)) {
-                // console.log("Invalid infohash");
-                return false;
-            }
+      this.validateAndSaveTransaction()
+    } else if (infohash) {
+      if (!infohash || infohash.length !== 40 || !/^[0-9A-Fa-f]+$/.test(infohash)) return false
+      if (this.clients.webtorrent.torrents.find(torrent => torrent.infoHash === infohash)) return false
 
-            if (this.clients.webtorrent.torrents.find(torrent => torrent.infoHash === infohash)) {
-                // console.log('Torrent is already downloading');
-                return false;
-            }
+      console.log(infohash, 'Received')
 
-            console.log(infohash, "Received");
+      this.leech(infohash)
+    } else if (torrentPath) {
+      console.info('Bootstrapping transaction from torrent file', torrentPath)
 
-            this.leech(infohash);
-        } else if (torrentPath) {
-            console.info("Bootstrapping transaction from torrent file", torrentPath);
+      this.leech(torrentPath)
+    } else if (from && to && amount) {
+      this.body = {
+        nonce: Math.random(),
+        from,
+        to,
+        amount: amount / 1,
+        message: message ?? '',
+        prev: /*isGenesis ? [] : */clients.transactions.findUnusedUTXOs(from, amount)
+      }
 
-            this.leech(torrentPath);
-        } else if (from && to && amount) {
-            this.body = {
-                nonce: Math.random(),
-                from: from,
-                to: to,
-                amount: amount / 1,
-                message: message ?? '',
-                prev: /*isGenesis ? [] : */clients.transactions.findUnusedUTXOs(from, amount),
-            }
-        
-            this.hash = ethUtil.sha256(Buffer.from(JSON.stringify(this.body))).toString('hex');
-            this.signature = clients.wallet.signHash(this.hash);
+      this.hash = ethUtil.sha256(Buffer.from(JSON.stringify(this.body))).toString('hex')
+      this.signature = clients.wallet.signHash(this.hash)
 
-            this.content = { tx: this.body, signature: this.signature, hash: this.hash }
-            this.txContentString = JSON.stringify(this.content, null, 4);
+      this.content = { tx: this.body, signature: this.signature, hash: this.hash }
+      this.txContentString = JSON.stringify(this.content, null, 4)
 
-            this.validateAndSaveTransaction();
-        }
+      this.validateAndSaveTransaction()
     }
+  }
 
     isValid() {
         if (this.hash === this.genesisHash)
